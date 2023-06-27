9bc8a42281f5b37d6688d2ea1bc501ba2419b68b
timothyportnoff <timothyportnoff@gmail.com>
2023-06-26 23:23:39 -0700

    Added sleeps, and made grab_bridge return a dictionary

--- main.py
+++ main.py
@@ -8,13 +8,38 @@ def grab_bridge():
     data = response.json()
 
     # Example: Print the first bridge's ID
-    if len(data) > 0:
+    if len(data) > 0: 
         first_bridge = data[0]
         bridge_id = first_bridge["id"]
-        print("Bridge ID:", bridge_id)
+        bridge_ip = first_bridge["internalipaddress"]
+        bridge_port = first_bridge["port"]
+
+        #Create a dictionary with the bridge information
+        bridge_data = {
+            "bridge_id": bridge_id,
+            "bridge_ip": bridge_ip,
+            "bridge_port": bridge_port
+        }
+        return bridge_data
     else:
         print("No bridges found.")
 
+#Execute if script is ran directly and not imported as module.
 if __name__ == "__main__":
+    #Welcome!
     print("Welcome to Lumi!")
-    grab_bridge()
+    sleep(0.5)
+
+    #Grab bridge information as a dictionary
+    bridge_data = grab_bridge()
+    if len(bridge_data) == 0:
+        print("Uh oh, spaghettio. Nothing in the bridge dict.")
+    print("Bridge ID:", bridge_data["bridge_id"])
+    print("Bridge IP:", bridge_data["bridge_ip"])
+    print("Bridge Port:", bridge_data["bridge_port"])
+    sleep(0.5)
+
+    #Connect to clip API
+    clip_url = "https://" + bridge_data["bridge_ip"] + "/debug/clip.html"
+    print("Clip API URL: " + clip_url)
+    sleep(0.5)

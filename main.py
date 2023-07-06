0141f71ffcd0ce02869ec9d78048967bcd417663
timothyportnoff <timothyportnoff@gmail.com>
2023-07-06 03:34:24 -0700

    Program is now slightly functional, lol. I added functionality for turning a singlular light (The one next to my desk) On and off. All menu options work, except for the one that dumps all hue bridge information. I probably have some sensitive hue bridge and light data in there. However, you'd need my API key which is generated locally to even get in. Good luck, hackers.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import urllib3
 import requests
 import json
 
-username = ''
-clientkey= ''
+api_key = ''
+client_key= ''
 
 def grab_bridge():
     # Access the JSON data
@@ -29,18 +29,24 @@ def grab_bridge():
         }
         return bridge_data
 
+#This will become the options menu, where we can determine length and all. 
+#menu_options = ['apple','orange','banana']
+    # for option in menu_options:
+    #     print(option)
+
 def print_menu():
     print("Lumi Options:")
     print("1. Authenticate user.")
     print("2. Print user authentication")
     print("3. Print all light info")
-    print("4. Option 4")
-    print("5. Quit")
+    print("4. Turn on light")
+    print("5. Turn off light")
+    print("0. Quit")
 
 def get_user_choice():
     while True:
         choice = input("Enter your choice (1-5): ")
-        if choice.isdigit() and 0 <= int(choice) <= 4:
+        if choice.isdigit() and 0 <= int(choice) <= 5:
             return int(choice)
         else:
             print("Invalid input. Please enter a valid choice.")
@@ -77,29 +83,28 @@ if __name__ == "__main__":
             if user_choice == 1: # Authenticate user
                 try: 
                     print("You chose Option 1")
-                    #TODO This will only work once the Hue bridge is pressed, so we should do this in a loop that waits for a minute for the user to press the button.
-                    #Sends 
+                    #Send message for our application for api_key and client_key 
                     message_body = { "devicetype":"app_name#instance_name", "generateclientkey":True }
 
                     #This url is the post URL to grab an authorized user
                     url = "https://" + bridge_data["bridge_ip"] + "/api"
 
-                    # Convert the message body to JSON TODO: This is probably a great idea
-                    json_body = json.dumps(message_body)
-
+                    #Atttempt to send request for max_attempt seconds for the user to press the button.
                     max_attempts = 8
                     for attempts in range(max_attempts):
-                        # Send a POST request to the Clip Debugger API
-                        response = requests.post(url, data=json_body, verify=False)
+                        # Send a POST request to the Clip Debugger APU URL with converted JSON message body
+                        response = requests.post(url, data=json.dumps(message_body), verify=False)
                         data = response.json()
 
                         # Check the response status
                         if response.status_code == 200: # Print the response content
                             if "success" in data[0]:
                                 print("Authorization Successful! Response: ", response.text) 
-                                username = data[0]["success"]["username"]
-                                clientkey = data[0]["success"]["clientkey"]
+                                api_key = data[0]["success"]["username"]
+                                client_key = data[0]["success"]["clientkey"]
                                 break
+                            elif "error" in data[0]:
+                                pass
                         if attempts == max_attempts - 1:
                             print("Failed to POST authentication request. Error code:", response.status_code, "Response: ", response.text)
                         sleep(1)
@@ -108,17 +113,15 @@ if __name__ == "__main__":
 
             elif user_choice == 2:
                 print("You chose Option 2")
-                print("Username: ", username)
-                print("ClientKey", clientkey)
-                # Perform actions for Option 2
+                print("Api Key: ", api_key)
+                print("Client Key", client_key)
 
             elif user_choice == 3:
                 print("You chose Option 3")
-                # Perform actions for Option 3
-                #url = "https://" + bridge_data["bridge_ip"] + "/api" + username + "/lights"
+                #url = "https://" + bridge_data["bridge_ip"] + "/api" + api_key + "/lights"
                 url = "https://" + bridge_data["bridge_ip"] + "/clip/v2/resource/device"
                 # response = requests.post(url, data=json_body, verify=False)
-                message_body = { "hue-application-key":username }
+                message_body = { "hue-application-key":api_key }
                 json_body = json.dumps(message_body)
                 # response = requests.get(url, verify = False)
                 # print(response)
@@ -127,7 +130,35 @@ if __name__ == "__main__":
 
             elif user_choice == 4:
                 print("You chose Option 4")
-                # Perform actions for Option 3
+                light="250c6c09-b90c-41f6-aec4-9f5e1d871edd" #This specific light is my personal RIGHT HIGH lamp
+                url = "https://" + bridge_data["bridge_ip"] + "/clip/v2/resource/light/" + light
+                headers = { "hue-application-key": api_key, "Content-Type": "application/json" }
+                payload = { "on": { "on": False } }
+                
+                #Send put request to URL with api_key and JSON converted message 
+                response = requests.put(url, headers=headers, data=json.dumps(payload), verify=False)
+
+                #Handle response code
+                if response.status_code == 200:
+                    print("PUT request successful. Light ",light, "turned on.")
+                else:
+                    print("PUT request failed. Error code:", response.status_code)
+
+            elif user_choice == 5:
+                print("You chose Option 5")
+                light="250c6c09-b90c-41f6-aec4-9f5e1d871edd" #This specific light is my personal RIGHT HIGH lamp
+                url = "https://" + bridge_data["bridge_ip"] + "/clip/v2/resource/light/" + light
+                headers = { "hue-application-key": api_key, "Content-Type": "application/json" }
+                payload = { "on": { "on": True } }
+                
+                #Send put request to URL with api_key and JSON converted message 
+                response = requests.put(url, headers=headers, data=json.dumps(payload), verify=False)
+
+                #Handle response code
+                if response.status_code == 200:
+                    print("PUT request successful. Light ",light, "turned off.")
+                else:
+                    print("PUT request failed. Error code:", response.status_code)
 
             elif user_choice == 0:
                 print("You chose to exit the program.")
@@ -142,9 +173,4 @@ if __name__ == "__main__":
     
     except requests.RequestException as e:
         print("An error occurred during the request:", str(e))
-
-
-
-
-
 
